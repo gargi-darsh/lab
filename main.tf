7a4535dc33e582c28441b2fd3d21b30ebd82a60c
Yash-Marke <darshanimarke8@gmail.com>
2021-11-09 13:02:19 +0530

    mycode

--- main.tf
+++ main.tf
@@ -17,3 +17,33 @@ image = "debian-cloud/debian-10"
 }
 }
 }
+
+resource "google_compute_instance" "myvm1" {
+    name = "mysecondvm"
+    zone = "us-central1-a"
+    machine_type = "f1-micro"
+    network_interface {
+network = "default"
+access_config{}
+}
+boot_disk {
+initialize_params {
+image = "debian-cloud/debian-10"
+}
+}
+}
+
+resource "google_compute_instance" "myvm3" {
+    name = "mythirdvm"
+    zone = "us-central1-a"
+    machine_type = "f1-micro"
+    network_interface {
+network = "default"
+access_config{}
+}
+boot_disk {
+initialize_params {
+image = "debian-cloud/debian-10"
+}
+}
+}
